1c9f00c5a036f77d6483c6e63d3adeec23e40877
verbik <verbikyullya@gmail.com>
2024-02-05 22:58:47 +0200

    Added view sets for manufacturer and country model

--- products/views.py
+++ products/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from products.models import Manufacturer, Country
+from products.serializers import (
+    ManufacturerListSerializer,
+    ManufacturerDetailSerializer,
+    ManufacturerSerializer,
+    CountrySerializer,
+)
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+
+class ManufacturerViewSet(viewsets.ModelViewSet):
+    queryset = Manufacturer.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ManufacturerListSerializer
+
+        if self.action == "retrieve":
+            return ManufacturerDetailSerializer
+
+        return ManufacturerSerializer
