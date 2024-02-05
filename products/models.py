29f081801ee9b512adfbf0ee87120743c9de8d98
verbik <verbikyullya@gmail.com>
2024-02-05 21:39:25 +0200

    Added manufacturer model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255)
+    info = models.TextField(null=True, blank=True)
+    country_of_origin = models.CharField(max_length=255)
+    website = models.URLField()
