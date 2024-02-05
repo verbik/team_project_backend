eeb4a25499e2140b727594c869ac803f07c0828e
verbik <verbikyullya@gmail.com>
2024-02-05 23:11:36 +0200

    Added basic serializer for wine

--- products/serializers.py
+++ products/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from products.models import Manufacturer, Country
+from products.models import Manufacturer, Country, Beverage, Wine
 
 
 class CountrySerializer(serializers.ModelSerializer):
@@ -36,3 +36,31 @@ class ManufacturerDetailSerializer(ManufacturerSerializer):
     country_of_origin = serializers.SlugRelatedField(
         many=False, read_only=True, slug_field="name"
     )
+
+
+class BeverageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Beverage
+        fields = (
+            "id",
+            "name",
+            "price",
+            "description",
+            "manufacturer",
+            "product_code",
+            "country",
+            "region",
+            "alcohol_content",
+            "volume",
+        )
+
+
+class WineSerializer(BeverageSerializer):
+    class Meta:
+        model = Wine
+        fields = BeverageSerializer.Meta.fields + (
+            "year",
+            "sugar_content",
+            "color",
+            "grape_variety",
+        )
