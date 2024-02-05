04a936febbb5b97b0f8e4b5cf9bddd9f51660526
verbik <verbikyullya@gmail.com>
2024-02-05 22:58:23 +0200

    Added serializer for country

--- products/serializers.py
+++ products/serializers.py
@@ -3,7 +3,14 @@ from rest_framework import serializers
 from products.models import Manufacturer, Country
 
 
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = "__all__"
+
+
 class ManufacturerSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Manufacturer
         fields = (
@@ -13,6 +20,16 @@ class ManufacturerSerializer(serializers.ModelSerializer):
             "country_of_origin",
             "website",
         )
+
+
+class ManufacturerListSerializer(serializers.ModelSerializer):
+    country_of_origin = serializers.SlugRelatedField(
+        many=False, read_only=True, slug_field="name"
+    )
+
+    class Meta:
+        model = Manufacturer
+        fields = ("id", "name", "country_of_origin")
 
 
 class ManufacturerDetailSerializer(ManufacturerSerializer):
