aea093c1575856031189cfd48269be7ee4daaef7
verbik <verbikyullya@gmail.com>
2024-02-20 20:25:47 +0200

    Modified models a bit, changed creation of order logic so that user can't create new order if old one is unpaid

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,41 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from orders.models import Order
-from orders.serializers import OrderSerializer
+from orders.serializers import (
+    OrderSerializer,
+    OrderListSerializer,
+    OrderAdminListSerializer,
+    OrderDetailSerializer,
+)
 
 
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
+    def get_queryset(self):
+        queryset = Order.objects.all()
+        request_user = self.request.user
+
+        if not request_user.is_staff:
+            queryset = queryset.filter(user_id=request_user.id)
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            if self.request.user.is_staff:
+                return OrderAdminListSerializer
+            return OrderListSerializer
+
+        if self.action == "retrieve":
+            return OrderDetailSerializer
+
+        return OrderSerializer
+
     def perform_create(self, serializer):
+        serializer.is_valid(raise_exception=True)
+
+        # Proceed with creating the new order
         serializer.save(user=self.request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
