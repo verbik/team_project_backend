004be3935aafbe9d37619b7e197b4f838cd9ca4d
verbik <verbikyullya@gmail.com>
2024-02-20 20:44:19 +0200

    Started new app for comments logic and created comment model

---
+++ comments/models.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
+    )
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+    created_at = models.DateTimeField(auto_now_add=True)
+    comment_contents = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Comment by {self.user} posted {self.created_at}"
