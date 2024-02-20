acf7aa7287f6ecd5ad0a2e997b018ec1a221c528
verbik <verbikyullya@gmail.com>
2024-02-21 00:05:25 +0200

    Basic logic for comments implemented: - add comments for wine instance - retrieve comments in wine detail serializer for this wine

--- comments/models.py
+++ comments/models.py
@@ -8,11 +8,16 @@ class Comment(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
     )
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(
+        ContentType, on_delete=models.CASCADE, related_name="comments"
+    )
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
     created_at = models.DateTimeField(auto_now_add=True)
     comment_contents = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["-created_at"]
 
     def __str__(self):
         return f"Comment by {self.user} posted {self.created_at}"
