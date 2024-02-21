ae5ecf7e8ab64252d758002840dec46f4eceda5d
verbik <verbikyullya@gmail.com>
2024-02-21 10:38:55 +0200

    Admin can modify/delete all comments and user can modify/delete only his own comments

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from comments.models import Comment
+from comments.permissions import IsOwnerOrReadOnly
+from comments.serializers import CommentSerializer
+
+
+class CommentViewSet(
+    GenericViewSet,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        queryset = Comment.objects.all()
+        request_user = self.request.user
+
+        if not request_user.is_staff:
+            queryset = queryset.filter(user=request_user)
+
+        return queryset
